dd0a9f866c3273a8545596b4ee6f2928d72af689
Renzo <renzomoretti2006@gmail.com>
2026-04-09 17:12:09 -0300

    First release

---
+++ src/jokers/thejoker.lua
@@ -0,0 +1,33 @@
+SMODS.Joker{
+    key = 'MainJoker',
+    atlas = 'placeholders',
+    pos = {
+        x = 0,
+        y = 0
+    },
+    config = {
+        extra = {
+            chips = 0,
+            mult = 0
+        }
+    },
+    rarity = 1,
+    cost = 1,
+    blueprint_compat = true,
+    loc_vars = function(self, info_queue, card)
+        return {
+            vars = {
+                card.ability.extra.chips,
+                card.ability.extra.mult
+            }
+        }
+    end,
+    calculate = function(self, card, context)
+        if context.joker_main then
+            return {
+                chips = card.ability.extra.chips,
+                mult = card.ability.extra.mult
+            }
+        end
+    end
+}
